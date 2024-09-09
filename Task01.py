309bc09636ec79a13d847ed2db41353d38f2dcab
PeshkovDmitry <peshkov-dv@yandex.ru>
2024-09-09 21:46:22 +0300

    Решено задание 1 через pytest

--- Task01.py
+++ Task01.py
@@ -1,33 +1,35 @@
-import unittest
+import pytest
 from Package01.BankAccount import BankAccount
 from Package01.InsufficientFundsError import InsufficientFundsError
 
 
-class TestBankAccount(unittest.TestCase):
+def test_constructor():
+    bank_account = BankAccount(100)
+    assert bank_account.get_balance() == 100, "Конструктор не создает баланс"
 
-    def test_constructor(self):
-        bank_account = BankAccount(100)
-        self.assertEqual(bank_account.get_balance(), 100, "Конструктор не создает баланс")
 
-    def test_default_constructor(self):
-        bank_account = BankAccount()
-        self.assertEqual(bank_account.get_balance(), 0, "Конструктор по умолчанию создает ненулевой баланс")
+def test_default_constructor():
+    bank_account = BankAccount()
+    assert bank_account.get_balance() == 0, "Конструктор по умолчанию создает ненулевой баланс"
 
-    def test_deposit(self):
-        bank_account = BankAccount(100)
-        bank_account.deposit(50)
-        self.assertEqual(bank_account.get_balance(), 150, "Деньги на депозит не кладуться")
 
-    def test_withdraw(self):
-        bank_account = BankAccount(100)
-        bank_account.withdraw(50)
-        self.assertEqual(bank_account.get_balance(), 50, "Деньги с депозита не снимаются")
+def test_deposit():
+    bank_account = BankAccount(100)
+    bank_account.deposit(50)
+    assert bank_account.get_balance() == 150, "Деньги на депозит не кладуться"
 
-    def test_withdraw_with_error(self):
-        bank_account = BankAccount(100)
-        self.assertRaises(InsufficientFundsError, bank_account.withdraw, 150)
 
+def test_withdraw():
+    bank_account = BankAccount(100)
+    bank_account.withdraw(50)
+    assert bank_account.get_balance() == 50, "Деньги с депозита не снимаются"
+
+
+def test_withdraw_with_error():
+    bank_account = BankAccount(100)
+    with pytest.raises(InsufficientFundsError):
+        bank_account.withdraw(150)
 
 
 if __name__ == "__main__":
-    unittest.main()
+    pytest.main(['-v'])
